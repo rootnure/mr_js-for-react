3bd34e04dc45f787090bfe7742da69491e80dd2f
rootnure <nur.diu.2791@gmail.com>
2026-03-01 17:54:28 +0600

    Template String, Arrow Function, Spread and Rest Operator

---
+++ template.js
@@ -0,0 +1,34 @@
+// // template string
+// const firstName = "Hero";
+// const lastName = "Alam";
+
+// // const fullName ="Welcome Back: " + firstName + " " + lastName;
+// const fullName = `Welcome Back: ${firstName} ${lastName}`;
+
+// console.log(fullName);
+
+
+
+// // arrow function
+// // function add(a, b) {
+// //     return a + b;
+// // }
+
+// // const sum = (a, b) => a+b;
+
+// const sum = (...rest) => {
+//     // console.log(arguments); // ❌
+//     console.log(rest); // ✅
+// }
+
+// console.log(sum(1, 2));
+
+
+
+// spread operator
+const numbers = [1, 2, 5, 6, 8];
+
+// console.log(...numbers);
+
+const newNumbers = [...numbers, 7, 9];
+console.log(newNumbers);
